d350874d84e55a42b0d761ff6eeaae28167e0a77
mohamedinfak <mohamedinfakstc@gmail.com>
2024-08-09 07:04:52 +0530

    feat:development completed

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,11 +1,11 @@
-import React, { createContext,useState}from "react"; 
-import all_product from "../components/Assets/all_product";
+import React, { createContext,useEffect,useState}from "react"; 
+
 
 export const ShopContext= createContext(null);
 
 const getDefaultCart =()=>{
     let cart={};
-    for(let index=0; index< all_product.length; index++){
+    for(let index=0; index< 300+1; index++){
        cart[index]=0;
 
     }
@@ -13,19 +13,56 @@ const getDefaultCart =()=>{
 }
 
 const ShopContextProvider = (props) => {
+
+    const [all_product,setAll_Product] = useState([]);
     const [cartItems,setCartItems]=useState(getDefaultCart());
   
+    useEffect(()=>{
+        fetch('http://localhost:4000/allproducts')
+        .then((response)=>response.json())
+        .then((data)=>setAll_Product(data))
+
+    },[])
+
     //console.log(cartItems)
 
     const addToCart=(itemId)=> {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+   //console.log(cartItems)
     }
     const removeFromCart=(itemId)=> {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
+const getTotalCartAmount = () => {
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    let totalAmount=0;
 
+    for(const item in cartItems )
+    {
+        if(cartItems[item]>0)
+        {
+            let itemInfo = all_product.find((product)=>product.id === Number(item))
+            totalAmount +=itemInfo.new_price * cartItems[item];
+            
+        }
+        
+    }
+    return totalAmount;
+}
+
+
+const getTotalCartItems= () =>{
+    let totalItem=0;
+    for(const item in cartItems){
+
+        if(cartItems[item]>0){
+            totalItem += cartItems[item];
+
+        }
+    }
+    return totalItem;
+}
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
         {props.children}
--- frontend/src/pages/ShopCatergory.jsx
+++ frontend/src/pages/ShopCatergory.jsx
@@ -7,16 +7,17 @@ const ShopCatergory = (props) => {
   const {all_product}= useContext(ShopContext)
   return (
     <div className='shop-catergory'>
-<img src={props.banner} alt="" />
+<img className='shopcatergory-banner' src={props.banner} alt="" />
 <div className="shopcategory-indextSort">
   <p>
     <span>Showing 1-12</span> out of 36 products
   </p>
   <div className="shopcategory-sort">
-    Sort by <img  className='shopcatergory-banner'src={dropdown_icon} alt="" />
+    Sort by <img  src={dropdown_icon} alt="" />
   </div>
 </div>
 <div className="shopcategory-products">
+  
   {all_product.map((item,i)=>{
     if(props.category===item.category){
       return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
